83034b5b540059f07d3ca2a912a25eaa005f857d
duxet <duxetlg@gmail.com>
2020-02-13 11:24:47 +0100

    Fix replies order

--- backend/lib/strm/entries/entries.ex
+++ backend/lib/strm/entries/entries.ex
@@ -3,19 +3,24 @@ defmodule Strm.Entries do
 
   alias Strm.Repo
   alias Strm.Entries.Entry
+  alias Strm.Entries.EntryReply
 
   def find_entry(id) do
     Entry |> Repo.get(id)
   end
 
   def list_entries(cursor \\ nil) do
+    replies_query = from r in EntryReply,
+      order_by: r.created_at,
+      preload: [:user]
+
     Entry
       |> limit(10)
       |> order_by([desc: :created_at])
       |> without_private()
       |> paginate(cursor)
       |> Repo.all()
-      |> Repo.preload([replies: [:user], group: [], user: []])
+      |> Repo.preload([replies: replies_query, group: [], user: []])
   end
 
   def create_entry(user, group, args) do

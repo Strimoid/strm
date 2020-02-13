b591ca819ba598a948ed3c573826a62dc178e873
duxet <duxetlg@gmail.com>
2020-02-13 19:11:30 +0100

    Add group input

--- frontend/components/entry/EntryForm.js
+++ frontend/components/entry/EntryForm.js
@@ -1,6 +1,10 @@
 export default (props) => (
     <div>
-        <textarea className="w-full border rounded p-4" placeholder="Dodaj wpis..."></textarea>
-        <button className="rounded bg-blue-500 hover:bg-blue-700 py-2 px-4 text-white float-right">Dodaj</button>
+        <textarea className="w-full border rounded p-4" placeholder="Add new entry..."></textarea>
+        
+        <div className="flex justify-end mt-1">
+            <input type="text" className="border rounded px-4 mr-4" placeholder="Group" />
+            <button className="rounded bg-blue-500 hover:bg-blue-700 py-2 px-4 text-white float-right">Add</button>
+        </div>
     </div>
 )

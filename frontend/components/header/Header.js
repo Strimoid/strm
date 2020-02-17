6a9810ef717e494e78fe1ba80642f78cb922ec5a
duxet <duxetlg@gmail.com>
2020-02-17 14:21:03 +0100

    Add i18n

--- frontend/components/header/Header.js
+++ frontend/components/header/Header.js
@@ -1,4 +1,5 @@
 import { useQuery } from '@apollo/react-hooks';
+import { FormattedMessage } from 'react-intl';
 import Link from 'next/link'
 import gql from "graphql-tag";
 
@@ -16,15 +17,19 @@ function Header({ token }) {
     return (
         <header className="w-full bg-blue-600 text-blue-500 px-4 pt-4 pb-0 mb-4 border-box">
             <Link href="/">
-                <a className="bg-white px-4 py-2 mr-4">Contents</a>
+                <a className="bg-white px-4 py-2 mr-4">
+                    <FormattedMessage id="header.contents" defaultMessage="Contents" />
+                </a>
             </Link>
             <Link href="/entries">
-                <a className="bg-white px-4 py-2">Entries</a>
+                <a className="bg-white px-4 py-2">
+                    <FormattedMessage id="header.entries" defaultMessage="Entries" />
+                </a>
             </Link>
     
             { data
                 ? <a className="bg-white px-4 py-2 float-right">{data.me.name}</a>
-                : <Link href="/sign-in"><a className="bg-white px-4 py-2 float-right">Sign in</a></Link>
+                : <Link href="/sign-in"><a className="bg-white px-4 py-2 float-right"><FormattedMessage id="header.sign-in" defaultMessage="Sign in" /></a></Link>
             }
             
         </header>
